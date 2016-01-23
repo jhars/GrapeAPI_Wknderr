86b9212e6471476ed00559460c67b87324809e0a
jhars <jjharlan06@gmail.com>
2016-01-23 12:50:03 -0800

    basic app/api running, nextStep: handle errors

---
+++ app/api/converter/currency.rb
@@ -0,0 +1,39 @@
+module Converter
+	class Currency < Grape::API
+		version 'v1', using: :path
+		format :json
+		rescue_from :all
+		# Pick Up Here!
+		# error_formatter :json, lambda { |
+		# }
+		helpers do
+			def get_exchange_rate(currency)
+				case currency
+				when 'NTD'
+					30
+				else
+					# ERROR HANDLING
+					# Probably want Status Code and/or Success or Failure Message
+					raise StandardError.new "no conversion found for currency '#{currency}' "
+				end
+			end
+
+		end
+
+		resource :converter do
+			params do
+				requires :amount, type: Float
+				requires :to_currency, type: String
+			end
+
+			get :exchange do
+				converted_amount = params[:amount] * get_exchange_rate(params[:to_currency])
+				{
+					amount: converted_amount,
+					currency: params[:to_currency]
+				}
+			end
+		
+		end
+	end
+end
